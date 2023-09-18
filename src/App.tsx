cc7b56dbf2e83cd19a233789537d62b35d32740b
tamta123 <tamtag186@gmail.com>
2023-09-18 11:42:50 +0400

    feat:create pages and routing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import Routing from "./components/Routing";
+
 function App() {
-  return <></>;
+  return (
+    <Main>
+      <Routing />
+    </Main>
+  );
 }
 
 export default App;
+
+const Main = styled.main`
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+`;
---
+++ src/components/Routing.tsx
@@ -0,0 +1,30 @@
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("../pages/Home"));
+const Detail = lazy(() => import("../pages/Detail"));
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={"loading..."}>
+            <Home />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/Detail"
+        element={
+          <Suspense fallback={"loading..."}>
+            <Detail />
+          </Suspense>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default Routing;
